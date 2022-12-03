0c9dc09d71274ab45f6710f2a0a3f451b6fc17b1
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-12-03 18:05:33 +0800

    Add Header

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header className="mt-[1em] text-center flex flex-col align-middle">
+      <img 
+        src="/dabaz.jpg" 
+        alt="DabAZ"
+        className="h-[8em] w-[8em] mx-auto rounded-full ring-4 ring-white shadow-xl"
+      />
+      <h1 className="mt-2 inline-block text-[3em] font-bold">
+        DabAZ
+      </h1>
+      <p className="-mt-1 inline-block text-[1.2em] text-gray-600">
+        I live my life.
+      </p>
+    </header>
+  )
+}
